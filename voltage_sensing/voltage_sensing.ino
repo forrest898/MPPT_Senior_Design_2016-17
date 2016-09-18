890a3f91640cd104954b265b9c9e1628dcc807da
Sean Turner <sean.turner1@maine.edu>
2016-09-18 17:31:40 -0400

    voltage sensing software started

---
+++ voltage_sensing/voltage_sensing.ino
@@ -0,0 +1,24 @@
+
+int analogPin = 3;
+int val = 0;
+float voltage = 0;
+float voltageRef = 4.9;
+float scale = 4.9;
+int resolution = 1020;
+
+void setup() {
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+}
+
+float voltage_read(int pin, float scale) {
+  val = analogRead(pin);
+  return((val&resolution) / (float)resolution * voltageRef * scale);
+}
+
+void loop() {
+  voltage = voltage_read(analogPin, scale);
+  Serial.println(voltage, 5);
+}
+
+
