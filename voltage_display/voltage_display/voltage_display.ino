376109f6adc3df0d199059f9394a37eda9ce92c4
forrest898 <fwsmith898@gmail.com>
2016-10-02 14:34:28 -0400

    voltage sensing plus display

---
+++ voltage_display/voltage_display/voltage_display.ino
@@ -0,0 +1,25 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 6, 7, 8);
+
+int analogPin = 3;
+int val = 0;
+float voltage = 0;
+float voltageRef = 4.9;
+float scale = 4.9;
+int resolution = 1020;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.noCursor();
+  analogReference(EXTERNAL);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
