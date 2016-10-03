49116c7f76d55809e6fd9a7276ba53337ad96bf8
forrest898 <fwsmith898@gmail.com>
2016-10-03 09:24:11 -0400

    Added current sense file

---
+++ current_sensing/current_sensing.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+
+}
+
+float current_read(int pin) {
+  val = analogRead(pin);
+  return((val&resolution) / (float)resolution * voltageRef);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
