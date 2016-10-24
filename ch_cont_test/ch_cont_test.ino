297c89820d635af270ed602473f0fd8906e10fec
forrest898 <fwsmith898@gmail.com>
2016-10-24 11:18:47 -0400

    double check to make sure everything from yesterday pushed

--- ch_cont_test/ch_cont_test.ino
+++ ch_cont_test/ch_cont_test.ino
@@ -1,5 +1,8 @@
-int fetSW = 3;
+#include <LiquidCrystal.h>
 
+LiquidCrystal lcd(12, 11, 5, 6, 7, 8);
+
+int fetSW = 3;
 int analogPin = 6;
 int val = 0;
 float voltage = 0;
@@ -7,7 +10,9 @@ float voltageRef = 5.02;
 float scale = 3.43;
 int resolution = 1020;
 
-void setup() {
+void setup() { 
+  lcd.begin(16, 2);
+  lcd.noCursor();
   pinMode(fetSW, OUTPUT);
   Serial.begin(9600);
   analogReference(EXTERNAL);
@@ -22,7 +27,22 @@ float voltage_read(int pin, float scale) {
 
 void loop() {
 
-  digitalWrite(fetSW, HIGH);
+  //digitalWrite(fetSW, HIGH);
+  //op_amp 
+  delay(1000);
+  lcd.clear();
+  voltage = voltage_read(analogPin, scale);
+  lcd.print(voltage);
+  lcd.print("\n");
+  Serial.println(voltage, 5);
+  if(voltage > 14.4) {
+    digitalWrite(fetSW, LOW);
+    lcd.print("Const V\n");
+  } else {
+    digitalWrite(fetSW, HIGH);
+    lcd.print("Const Curr\n");
+  }
+  
  
 }
 
