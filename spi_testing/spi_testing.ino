f8c67b3b378eb231c0497d9f50aa8947456cdf56
forrest898 <fwsmith898@gmail.com>
2016-10-26 13:38:42 -0400

    ADC works, yay

--- spi_testing/spi_testing.ino
+++ spi_testing/spi_testing.ino
@@ -6,9 +6,14 @@
 #define DATAIN 12    // MISO 
 #define SPICK 13  // Clock 
 
+SPISettings settings(100000, MSBFIRST, SPI_MODE0);
+
 void setup() {
   //set pin modes 
   pinMode(CS, OUTPUT); 
+  pinMode(DATAOUT, OUTPUT); 
+  pinMode(DATAIN, INPUT); 
+  pinMode(SPICK, OUTPUT);
   // disable device to start with 
   digitalWrite(CS, HIGH); 
 
@@ -20,8 +25,8 @@ int v;
 double voltage;
 void loop() {
   
-  v =read_adc(0);
-  voltage = (v*4.62) /4095.0;
+  v =read_adc(2);
+  voltage = (v*4.99) / 4095;
   Serial.print("Voltage on CH0 =");
   Serial.println(voltage, 5);
   delay(500);
@@ -31,10 +36,11 @@ int read_adc(int ch) {
   
   int adcval = 0;
 
-  SPI.setClockDivider(SPI_CLOCK_DIV16);
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE0);
-
+  //SPI.setClockDivider(SPI_CLOCK_DIV16);
+  
+  //SPI.setBitOrder(MSBFIRST);
+  //SPI.setDataMode(SPI_MODE0);
+  SPI.beginTransaction(settings);
   digitalWrite(CS, LOW);
   
   byte config = 0x0C;
@@ -44,10 +50,10 @@ int read_adc(int ch) {
   byte low = SPI.transfer(0x00);
 
   digitalWrite(CS, HIGH);
-
+  SPI.endTransaction();
   adcval = ((high & 0x0f) << 8) + low;
-
-  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  
+ // SPI.setClockDivider(SPI_CLOCK_DIV4);
 
   return adcval;
   
