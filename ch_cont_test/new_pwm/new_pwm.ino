fd443c84687b07188474cf2c68b085598a6a48f5
forrest898 <fwsmith898@gmail.com>
2016-11-02 17:02:38 -0400

    testing

--- ch_cont_test/new_pwm/new_pwm.ino
+++ ch_cont_test/new_pwm/new_pwm.ino
@@ -1,51 +1,90 @@
 #include <LiquidCrystal.h>
 #include <SPI.h>
-#include "C:\Users\Forrest\Documents\402\code\MPPT_Senior_Design_2016-17\lcd_display\adc.hpp"
-#include "C:\Users\Forrest\Documents\402\code\MPPT_Senior_Design_2016-17\lcd_display\pwm.hpp"
-#include "C:\Users\Forrest\Documents\402\code\MPPT_Senior_Design_2016-17\lcd_display\power_sensing.hpp"
-#include "C:\Users\Forrest\Documents\402\code\MPPT_Senior_Design_2016-17\lcd_display\lcd.hpp"
+#include "adc.hpp"
+#include "lcd.hpp"
+#include "power_sensing.hpp"
+#include "pwm.hpp"
 
 LiquidCrystal lcd(8,7,2,4,5,6);
 
 float V_REF = 5.02;
 float gain = 3.43;
-float readval;
+float readval, readval2;
 
 void setup() {
   lcd_init();
+  PWM_init();
   adc_init();
+  set_PWM_frequency(50000);
+  set_PWM_duty_p10(.75);
+  delay(20);
   //pinMode(3, INPUT);
   
-  pinMode(3, OUTPUT);
-  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
-  TCCR2B = _BV(CS22);
-  
-  OCR2B = 175;
-  
+ 
 }
 
 void loop() {
 
-  readval =  read_voltage(0, gain);
+  lcd.clear();
+  readval = read_voltage(0, gain);
+  lcd.print(readval, 2);
+  delay(100);
+  /*
+  readval = 0;
+  readval2 = 0;
+  
+  for(int i = 0; i < 10; i++) {
+   
+    readval +=  read_voltage(0, gain);
+    readval2 +=  read_current(1, 10);
+    
+    delay(10);
+  }
+
+  readval /= 10;
+  readval2 /= 10;
   lcd.clear();
   lcd.print(readval, 2);
-  delay(50);
+  lcd.print("\n");
+  lcd.print(readval2, 2);
+
+  */
   /*
   for(int i=0; i<200; i++) {
     OCR2B += 1;
-    readval =  read_voltage(0, gain);
+    for(int i = 0; i < 10; i++) {
+     
+      readval +=  read_voltage(0, gain);
+      readval2 +=  read_current(1, 10);
+      
+    }
+  
+    readval /= 10;
+    readval2 /= 10;
     lcd.clear();
     lcd.print(readval, 2);
+    lcd.print("\n");
+    lcd.print(readval2, 2);
     delay(20);
   }
   for(int i = 200; i > 0; i--) {
     OCR2B -= 1;
-    readval =  read_voltage(0, gain);
+     for(int i = 0; i < 10; i++) {
+     
+      readval +=  read_voltage(0, gain);
+      readval2 +=  read_current(1, 10);
+
+    }
+  
+    readval /= 10;
+    readval2 /= 10;
     lcd.clear();
     lcd.print(readval, 2);
+    lcd.print("\n");
+    lcd.print(readval2, 2);
     delay(20);
   }
-  */  
-
+   
+*/
 }
 
